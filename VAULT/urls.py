f02c02af3fcc2e358c0f7d6502a91e74889127a9
Britto69 <britto.fernandes@msds.christuniversity.in>
2024-01-30 12:19:17 +0530

    linking entire user page

--- VAULT/urls.py
+++ VAULT/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('b', include('home.urls')),
+    path('c', include('home.urls')),
     path('',include('User.urls')),
     
 
